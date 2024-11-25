ffba6763acb20a8d18fecc599683462afdede365
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-11-24 23:47:05 -0500

    Add FastEndpoints nuget package
    - Add empty example endpoint

---
+++ BaseballStats.WebApi/Endpoints/ExampleEndpoint/ExampleEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.ExampleEndpoint;
+
+public class ExampleEndpoint : Endpoint<EmptyRequest, EmptyResponse>
+{
+    public override void Configure()
+    {
+        Get("api/example");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync(EmptyRequest request, CancellationToken cancellationToken)
+    {
+        await SendAsync(new EmptyResponse(), cancellation: cancellationToken);
+    }
+}
--- BaseballStats.WebApi/Program.cs
+++ BaseballStats.WebApi/Program.cs
@@ -1,23 +1,30 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddFastEndpoints()
+    .SwaggerDocument();
 builder.Services.AddDbContext<AppDbContext>(options => { options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")); });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseFastEndpoints()
+    .UseSwaggerGen();
 
 app.UseHttpsRedirection();
 
