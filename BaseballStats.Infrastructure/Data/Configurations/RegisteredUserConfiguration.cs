642625831a902819989dd1967fa8fdceb27394a3
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-11-28 18:24:46 -0500

    Create identity entities configuration
    - Add RegisteredUser and configuration
    
    Issue #2

---
+++ BaseballStats.Infrastructure/Data/Configurations/RegisteredUserConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using BaseballStats.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class RegisteredUserConfiguration : IEntityTypeConfiguration<RegisteredUser>
+{
+    public void Configure(EntityTypeBuilder<RegisteredUser> builder)
+    {
+        builder.UseTptMappingStrategy();
+        
+        builder.Property(x => x.Username).HasMaxLength(255);
+
+        builder.Property(x => x.Password).HasMaxLength(255);
+        
+        builder.Property(x => x.Type);
+    }
+}
---
+++ BaseballStats.Infrastructure/Data/Configurations/TechnicalDirectorConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BaseballStats.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations;
+
+public class TechnicalDirectorConfiguration : IEntityTypeConfiguration<TechnicalDirector>
+{
+    public void Configure(EntityTypeBuilder<TechnicalDirector> builder)
+    {
+    }
+}
