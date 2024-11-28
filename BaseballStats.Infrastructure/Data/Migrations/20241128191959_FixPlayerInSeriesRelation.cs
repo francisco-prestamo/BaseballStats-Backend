a9fc63fd9427a519fd235611f8fa47a159841ff2
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-11-28 14:25:01 -0500

    Add FixPlayerInSeriesRelation

---
+++ BaseballStats.Infrastructure/Data/Migrations/20241128191959_FixPlayerInSeriesRelation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPlayerInSeriesRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerInSeries_TeamId",
+                table: "PlayerInSeries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerInSeries_TeamId",
+                table: "PlayerInSeries",
+                column: "TeamId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerInSeries_TeamId",
+                table: "PlayerInSeries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerInSeries_TeamId",
+                table: "PlayerInSeries",
+                column: "TeamId",
+                unique: true);
+        }
+    }
+}
