4423735593c0da4a500100b9d15615bb59e0763a
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2024-11-28 20:48:42 -0500

    Add RegisteredUserEntity Migration
    Issue #2

---
+++ BaseballStats.Infrastructure/Data/Migrations/20241129013407_AddRegisteredUserEntity.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegisteredUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "TechnicalDirector");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "TechnicalDirector");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "TechnicalDirector");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "TechnicalDirector",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.CreateTable(
+                name: "RegisteredUser",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegisteredUser", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TechnicalDirector_RegisteredUser_Id",
+                table: "TechnicalDirector",
+                column: "Id",
+                principalTable: "RegisteredUser",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TechnicalDirector_RegisteredUser_Id",
+                table: "TechnicalDirector");
+
+            migrationBuilder.DropTable(
+                name: "RegisteredUser");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "TechnicalDirector",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "TechnicalDirector",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "TechnicalDirector",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "TechnicalDirector",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
