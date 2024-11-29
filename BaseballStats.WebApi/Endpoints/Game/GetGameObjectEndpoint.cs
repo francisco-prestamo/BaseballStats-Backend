df0b36e4bd0b4a79f33a51a51b17cf37d019c639
Eduardo Brito Labrada <eblabrada@gmail.com>
2024-11-29 02:45:43 -0500

    Add GameObjectEndpoint
    Issue #13

---
+++ BaseballStats.WebApi/Endpoints/Game/GetGameObjectEndpoint.cs
@@ -0,0 +1,20 @@
+using FastEndpoints;
+using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.Game.GetGameObject;
+
+namespace BaseballStats.WebApi.Endpoints.Game;
+
+public class GetGameObjectEndpoint : Endpoint<GetGameObjectCommand, GameDto>
+{
+    public override void Configure()
+    {
+        Get("games/{GameId}");
+        Summary(x => x.Summary = "Obtiene el juego cuyo id es gameId");
+    }
+
+    public override async Task HandleAsync(GetGameObjectCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
