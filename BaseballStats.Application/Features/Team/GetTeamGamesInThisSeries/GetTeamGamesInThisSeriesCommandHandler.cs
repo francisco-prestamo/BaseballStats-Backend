a614ddf3d8f35e4a6c090da77f4f5ecb87eb87cb
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-18 11:57:30 -0500

    - add new database validations

--- BaseballStats.Application/Features/Team/GetTeamGamesInThisSeries/GetTeamGamesInThisSeriesCommandHandler.cs
+++ BaseballStats.Application/Features/Team/GetTeamGamesInThisSeries/GetTeamGamesInThisSeriesCommandHandler.cs
@@ -63,5 +63,27 @@ public class GetTeamGamesInThisSeriesCommandHandler(IUnitOfWork unitOfWork) : Co
 
         if (series is null)
             ThrowError("SeriesId not found", StatusCodes.Status404NotFound);
+
+        if (series.SeasonId != command.SeasonId)
+            ThrowError("SeriesId doesn't belong to SeasonId", StatusCodes.Status400BadRequest);
+
+        var gameRepository = unitOfWork.Repository<Domain.Entities.Game>();
+
+        var relevantTeamIds = gameRepository.Where(x => x.SeriesId == command.SeriesId)
+            .Select(x => x.Team1Id)
+            .Union(gameRepository.Where(x => x.SeriesId == command.SeriesId)
+            .Select(x => x.Team2Id))
+            .Distinct()
+            .ToList();
+
+        bool found = false;
+        foreach (var relevantTeamId in relevantTeamIds)
+        {
+            if (relevantTeamId == command.TeamId)
+                found = true;
+        }
+
+        if (!found)
+            ThrowError("TeamId didn't play in SeriesId", StatusCodes.Status400BadRequest);
     }
 }
