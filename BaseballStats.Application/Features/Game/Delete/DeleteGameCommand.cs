923d624e1c811cefbbd1d9a8a5811a25e839e221
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-19 02:11:52 -0500

    feat: add DeleteGameEndpoint

---
+++ BaseballStats.Application/Features/Game/Delete/DeleteGameCommand.cs
@@ -0,0 +1,9 @@
+﻿using BaseballStats.Application.DTOs;
+using FastEndpoints;
+
+namespace BaseballStats.Application.Features.Game.Delete;
+
+public class DeleteGameCommand : ICommand<GameDto>
+{
+    public long GameId { get; set; }
+}
---
+++ BaseballStats.WebApi/Endpoints/Game/DeleteGamesEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.Game.Delete;
+using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.Game;
+
+public class DeleteGamesEndpoint : Endpoint<DeleteGameCommand, GameDto>
+{
+    public override void Configure()
+    {
+        Delete("/games/{GameId}");
+        AllowAnonymous();
+        Summary(x => x.Summary = "Delete a game");
+    }
+
+    public override async Task HandleAsync(DeleteGameCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
