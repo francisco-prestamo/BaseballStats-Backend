91f80fe7ff1b1dc3ef922e1fae4677fdea485ed1
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-19 02:11:52 -0500

    feat: add DeleteGameEndpoint

---
+++ BaseballStats.Application/Features/Game/Delete/DeleteGameCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Mappers;
+using BaseballStats.Domain.Interfaces.DataAccess;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace BaseballStats.Application.Features.Game.Delete;
+
+public class DeleteGameCommandHandler(IUnitOfWork unitOfWork) : CommandHandler<DeleteGameCommand, GameDto>
+{
+    public override async Task<GameDto> ExecuteAsync(DeleteGameCommand command, CancellationToken ct = new CancellationToken())
+    {
+        await DatabaseValidation(command);
+        
+        var game = await unitOfWork.Repository<Domain.Entities.Game>().DeleteAsync(command.GameId);
+        
+        await unitOfWork.SaveChangesAsync(ct);
+
+        return game!.ToGameDto();
+    }
+
+    private async Task DatabaseValidation(DeleteGameCommand command)
+    {
+        var game = await unitOfWork.Repository<Domain.Entities.Game>().GetByIdAsync(command.GameId);
+
+        if (game == null)
+            ThrowError("Game not found", StatusCodes.Status404NotFound);
+    }
+}
---
+++ BaseballStats.Application/Features/Game/Delete/DeleteGameValidator.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace BaseballStats.Application.Features.Game.Delete;
+
+public class DeleteGameValidator : Validator<DeleteGameCommand>
+{
+    public DeleteGameValidator()
+    {
+        RuleFor(x => x.GameId)
+            .NotEmpty().WithMessage("GameId is required")
+            .GreaterThan(0).WithMessage("GameId must be greater than 0");
+    }
+}
