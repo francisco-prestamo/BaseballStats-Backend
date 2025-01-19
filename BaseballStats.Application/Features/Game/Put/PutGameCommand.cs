c1d2c0d01c41d02a9891b861afd81398a232afb6
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-19 02:29:24 -0500

    feat: add PutGameEndpoint

---
+++ BaseballStats.Application/Features/Game/Put/PutGameCommand.cs
@@ -0,0 +1,8 @@
+﻿using BaseballStats.Application.DTOs;
+using FastEndpoints;
+
+namespace BaseballStats.Application.Features.Game.Put;
+
+public record PutGameCommand : GameDto, ICommand<GameDto>
+{
+}
---
+++ BaseballStats.WebApi/Endpoints/Game/PutGameEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.Game.Put;
+using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.Game;
+
+public class PutGameEndpoint : Endpoint<PutGameCommand, GameDto>
+{
+    public override void Configure()
+    {
+        Put("/games/{Id}");
+        AllowAnonymous();
+        Summary(x => x.Summary = "Update a game");
+    }
+
+    public override async Task HandleAsync(PutGameCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
