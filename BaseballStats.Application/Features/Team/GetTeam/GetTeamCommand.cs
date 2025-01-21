6fea28acce8123651dd3582620237782ca2997bd
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-21 18:32:04 -0500

    refactor: GetTeam

--- BaseballStats.Application/Features/Team/GetTeam/GetTeamCommand.cs
+++ BaseballStats.Application/Features/Team/GetTeam/GetTeamCommand.cs
@@ -6,7 +6,5 @@ namespace BaseballStats.Application.Features.TeamNamespace.GetTeam;
 // ReSharper disable once ClassNeverInstantiated.Global
 public record GetTeamCommand : ICommand<TeamDto>
 {
-    public long SeasonId { get; init; }
-    public long SeriesId { get; init; }
     public long TeamId { get; init; }
 }
--- BaseballStats.Application/Features/Team/GetTeam/GetTeamValidator.cs
+++ BaseballStats.Application/Features/Team/GetTeam/GetTeamValidator.cs
@@ -10,13 +10,5 @@ public class GetTeamValidator : Validator<GetTeamCommand>
         RuleFor(x => x.TeamId)
             .NotEmpty().WithMessage("TeamId is required")
             .GreaterThan(0).WithMessage("TeamId must be greater than 0");
-
-        RuleFor(x => x.SeasonId)
-            .NotEmpty().WithMessage("SeasonId is required")
-            .GreaterThan(0).WithMessage("SeasonId must be greater than 0");
-        
-        RuleFor(x => x.SeriesId)
-            .NotEmpty().WithMessage("SeriesId is required")
-            .GreaterThan(0).WithMessage("SeriesId must be greater than 0");
     }
 }
