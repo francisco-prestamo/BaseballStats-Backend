c82a7c3e5cfa968674d9d8a3147a62e5564b0b8e
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-21 18:32:04 -0500

    refactor: GetTeam

--- BaseballStats.Application/Features/Team/GetTeam/GetTeamCommandHandler.cs
+++ BaseballStats.Application/Features/Team/GetTeam/GetTeamCommandHandler.cs
@@ -25,39 +25,5 @@ public class GetTeamCommandHandler(IUnitOfWork unitOfWork) : CommandHandler<GetT
 
         if (team is null)
             ThrowError("TeamId not found", StatusCodes.Status404NotFound);
-
-        var seasonRepository = unitOfWork.Repository<Domain.Entities.Season>();
-        var season = await seasonRepository.GetByIdAsync(command.SeasonId);
-
-        if (season is null)
-            ThrowError("SeasonId not found", StatusCodes.Status404NotFound);
-
-        var seriesRepository = unitOfWork.Repository<Domain.Entities.Series>();
-        var series = await seriesRepository.GetByIdAsync(command.SeriesId);
-
-        if (series is null)
-            ThrowError("SeriesId not found", StatusCodes.Status404NotFound);
-
-        if (series.SeasonId != command.SeasonId)
-            ThrowError("SeriesId doesn't belong to SeasonId", StatusCodes.Status400BadRequest);
-
-        var gameRepository = unitOfWork.Repository<Domain.Entities.Game>();
-
-        var relevantTeamIds = gameRepository.Where(x => x.SeriesId == command.SeriesId)
-            .Select(x => x.Team1Id)
-            .Union(gameRepository.Where(x => x.SeriesId == command.SeriesId)
-            .Select(x => x.Team2Id))
-            .Distinct()
-            .ToList();
-
-        bool found = false;
-        foreach (var relevantTeamId in relevantTeamIds)
-        {
-            if (relevantTeamId == command.TeamId)
-                found = true;
-        }
-
-        if (!found)
-            ThrowError("TeamId didn't play in SeriesId", StatusCodes.Status400BadRequest);
     }
 }
