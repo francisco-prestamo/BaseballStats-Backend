26ae0c09a2414255131002777236615535d619ae
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-21 16:52:34 -0500

    feat: add PostSeriesEndpoint

--- BaseballStats.Application/DTOs/SeriesDto.cs
+++ BaseballStats.Application/DTOs/SeriesDto.cs
@@ -7,11 +7,9 @@ public record SeriesDto
     public long IdSeason { get; init; }
 
     public string Name { get; init; } = null!;
-    public  string Type { get; init; } = null!;
+    public string Type { get; init; } = null!;
 
     public DateOnly StartDate { get; init; }
 
     public DateOnly EndDate { get; init; }
-
-
 }
---
+++ BaseballStats.Application/Features/Series/Post/PostSeriesValidator.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace BaseballStats.Application.Features.Series.Post;
+
+public class PostSeriesValidator : Validator<PostSeriesCommand>
+{
+    public PostSeriesValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+        
+        RuleFor(x => x.Type).NotEmpty().WithMessage("Type is required.");
+        
+        RuleFor(x => x.StartDate).NotEmpty().WithMessage("Start Date is required.");
+        
+        RuleFor(x => x.EndDate).NotEmpty().WithMessage("End Date is required.");
+        
+        RuleFor(x => x.SeasonId)
+            .NotEmpty().WithMessage("Season Id is required.")
+            .GreaterThan(0).WithMessage("Season Id must be greater than 0.");
+    }
+}
