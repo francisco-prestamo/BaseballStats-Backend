79857b9a30e3dff0362aaff74b7bc579a81ee29e
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-21 23:24:34 -0500

    feat: add Post and Delete Season Endpoints

---
+++ BaseballStats.Application/Features/Season/Delete/DeleteSeasonCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Mappers;
+using BaseballStats.Domain.Interfaces.DataAccess;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace BaseballStats.Application.Features.Season.Delete;
+
+public class DeleteSeasonCommandHandler(IUnitOfWork unitOfWork) : CommandHandler<DeleteSeasonCommand, SeasonDto>
+{
+    public override async Task<SeasonDto> ExecuteAsync(DeleteSeasonCommand command, CancellationToken ct = new CancellationToken())
+    {
+        await DatabaseValidationsAsync(command);
+
+        var seasonRepository = unitOfWork.Repository<Domain.Entities.Season>();
+
+        var season = await seasonRepository.DeleteAsync(command.Id);
+        
+        await unitOfWork.SaveChangesAsync(ct);
+
+        return season!.ToDto();
+    }
+
+    private async Task DatabaseValidationsAsync(DeleteSeasonCommand command)
+    {
+        var season = await unitOfWork.Repository<Domain.Entities.Season>().GetByIdAsync(command.Id);
+
+        if (season == null)
+            ThrowError("Season not found.", StatusCodes.Status404NotFound);
+    }
+}
---
+++ BaseballStats.WebApi/Endpoints/Season/PostSeasonEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.Season.Post;
+using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.Season;
+
+public class PostSeasonEndpoint : EndpointWithoutRequest<SeasonDto>
+{
+    public override void Configure()
+    {
+        Post("seasons/");
+        AllowAnonymous();
+        Summary(x => x.Summary = "Create a new season");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var response = await new PostSeasonCommand().ExecuteAsync(ct);
+        await SendAsync(response, 200, ct);
+    }
+}
