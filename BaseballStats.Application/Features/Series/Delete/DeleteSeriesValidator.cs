18fd419d8ff7ba826c166417451488f283bd6aea
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-21 20:30:29 -0500

    feat: add DeleteSeriesEndpoint

---
+++ BaseballStats.Application/Features/Series/Delete/DeleteSeriesValidator.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace BaseballStats.Application.Features.Series.Delete;
+
+public class DeleteSeriesValidator : Validator<DeleteSeriesCommand>
+{
+    public DeleteSeriesValidator()
+    {
+        RuleFor(x => x.SeasonId)
+            .NotEmpty().WithMessage("SeasonId is required.")
+            .GreaterThan(0).WithMessage("SeasonId must be greater than 0.");
+
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.")
+            .GreaterThan(0).WithMessage("Id must be greater than 0.");
+    }
+}
