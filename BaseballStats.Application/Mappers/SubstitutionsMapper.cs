795de489d7faa19798be28267a93dd9c58297d91
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-21 21:01:48 -0500

    feat: add substitution service fix: Get Substitutions Command Handler

--- BaseballStats.Application/Mappers/SubstitutionsMapper.cs
+++ BaseballStats.Application/Mappers/SubstitutionsMapper.cs
@@ -5,22 +5,12 @@ namespace BaseballStats.Application.Mappers;
 
 public static class SubstitutionsMapper
 {
-    public static SubstitutionsDto ToDto(this (List<Domain.Entities.Substitution>, List<Domain.Entities.Substitution>) substitutions)
+    public static SubstitutionsDto ToDto(this (List<DTOs.Substitution>, List<DTOs.Substitution>) substitutions)
     {
-        List<DTOs.Substitution> FormatSubstitutions(List<Domain.Entities.Substitution> substitutions)
-        {
-            List<DTOs.Substitution> result = new();
-            foreach (var substitution in substitutions)
-            {
-                result.Add(new(substitution.TeamId, substitution.PlayerInId, substitution.PlayerOutId, substitution.Time));
-            }
-            return result;
-        }
-
         return new SubstitutionsDto()
         {
-            Team1Substitutions = FormatSubstitutions(substitutions.Item1),
-            Team2Substitutions = FormatSubstitutions(substitutions.Item2)
+            Team1Substitutions = substitutions.Item1,
+            Team2Substitutions = substitutions.Item2
         };
     }
 }
--- BaseballStats.Application/Services/TeamWithExtrasService.cs
+++ BaseballStats.Application/Services/TeamWithExtrasService.cs
@@ -1,10 +1,12 @@
 ﻿using BaseballStats.Domain.Entities;
 using BaseballStats.Domain.Interfaces.DataAccess;
 using BaseballStats.Application.ResultSets;
+using BaseballStats.Domain.Enums;
+using BaseballStats.Application.Mappers;
 
 namespace BaseballStats.Application.Services;
 
-public class TeamWithExtrasService(IUnitOfWork unitOfWork)
+public class TeamWithExtrasService(AlignmentService alignmentsService, IUnitOfWork unitOfWork)
 {
     public Task<IEnumerable<TeamWithExtras>> GetTeamsWithExtrasAsync()
     {
@@ -12,23 +14,23 @@ public class TeamWithExtrasService(IUnitOfWork unitOfWork)
         var games = unitOfWork.Repository<Game>().DbSet;
 
         var teamsWithExtras = from team in teams
-            join game in games on team.Id equals game.Team1Id into team1Games
-            from game1 in team1Games.DefaultIfEmpty()
-            join game in games on team.Id equals game.Team2Id into team2Games
-            from game2 in team2Games.DefaultIfEmpty()
-            group new { team, game1, game2 } by team.Id
+                              join game in games on team.Id equals game.Team1Id into team1Games
+                              from game1 in team1Games.DefaultIfEmpty()
+                              join game in games on team.Id equals game.Team2Id into team2Games
+                              from game2 in team2Games.DefaultIfEmpty()
+                              group new { team, game1, game2 } by team.Id
             into g
-            select new TeamWithExtras
-            {
-                Id = g.Key,
-                Name = g.First().team.Name,
-                Initials = g.First().team.Initials,
-                Color = g.First().team.Color,
-                RepresentedEntity = g.First().team.RepresentedEntity,
-                TotalRuns = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? tg.game1.Runs1 : tg.game2.Runs2),
-                WinGames = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? (tg.game1.Winner1 ? 1 : 0) : (tg.game2.Winner1 ? 0 : 1)),
-                LoseGames = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? (tg.game1.Winner1 ? 0 : 1) : (tg.game2.Winner1 ? 1 : 0))
-            };
+                              select new TeamWithExtras
+                              {
+                                  Id = g.Key,
+                                  Name = g.First().team.Name,
+                                  Initials = g.First().team.Initials,
+                                  Color = g.First().team.Color,
+                                  RepresentedEntity = g.First().team.RepresentedEntity,
+                                  TotalRuns = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? tg.game1.Runs1 : tg.game2.Runs2),
+                                  WinGames = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? (tg.game1.Winner1 ? 1 : 0) : (tg.game2.Winner1 ? 0 : 1)),
+                                  LoseGames = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? (tg.game1.Winner1 ? 0 : 1) : (tg.game2.Winner1 ? 1 : 0))
+                              };
 
         return Task.FromResult(teamsWithExtras.AsEnumerable());
     }
@@ -59,12 +61,59 @@ public class TeamWithExtrasService(IUnitOfWork unitOfWork)
                     LoseGames = g.Sum(tg => tg.team.Id == tg.game1.Team1Id ? (tg.game1.Winner1 ? 0 : 1) : (tg.game2.Winner1 ? 1 : 0))
                 }
             )
-            join pis in playersInSeries on twe.Id equals pis.TeamId
-            where pis.SeriesId == seriesId
-            select twe;
+                              join pis in playersInSeries on twe.Id equals pis.TeamId
+                              where pis.SeriesId == seriesId
+                              select twe;
 
         var teamWithExtrasEnumerable = teamsWithExtras.AsEnumerable().DistinctBy(x => x.Id);
 
         return Task.FromResult(teamWithExtrasEnumerable);
+    }
+
+    public Task<IEnumerable<DTOs.Substitution>> GetSubstitutionsWithExtrasAsync(long gameId, long teamId)
+    {
+        var game = unitOfWork.Repository<Game>().GetByIdAsync(gameId)!;
+        var substitutionsRepository = unitOfWork.Repository<Substitution>();
+        var playerInPosition_table = unitOfWork.Repository<PlayerInPosition>().DbSet;
+        var player_table = unitOfWork.Repository<Player>().DbSet;
+
+        var playerInPosition =
+            (from p in playerInPosition_table
+             select new { p.PlayerId, p.Position, p.Effectiveness }).ToDictionary(x => (x.PlayerId, x.Position));
+
+        var alignment = alignmentsService.GetAlignmentsFromGame(gameId, teamId);
+
+        Dictionary<long, (PlayerPositions, double)> positions = new();
+        foreach (var player in alignment.Result)
+            positions.Add(player.Id, (player.Position, player.Effectiveness));
+
+        var substitutions = substitutionsRepository.Where(x => x.GameId == gameId && x.TeamId == teamId).ToList();
+        substitutions.Sort((x, y) => x.Time.CompareTo(y.Time));
+
+        List<DTOs.Substitution> ret = new();
+        foreach (var substitution in substitutions)
+        {
+            var playerOut = new PlayerInPosition
+            {
+                PlayerId = substitution.PlayerOutId,
+                Position = positions[substitution.PlayerOutId].Item1,
+                Effectiveness = positions[substitution.PlayerOutId].Item2,
+            };
+
+            var oldPosition = positions[substitution.PlayerOutId].Item1;
+            
+            positions.Add(substitution.PlayerInId, (oldPosition, playerInPosition[(substitution.PlayerInId, oldPosition)].Effectiveness));
+
+            var playerIn = new PlayerInPosition
+            {
+                PlayerId = substitution.PlayerInId,
+                Position = positions[substitution.PlayerInId].Item1,
+                Effectiveness = positions[substitution.PlayerInId].Item2,
+            };
+
+            ret.Add(new DTOs.Substitution(teamId, playerIn, playerOut, substitution.Time));
+        }
+
+        return Task.FromResult(ret.AsEnumerable());
     }
 }
