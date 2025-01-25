373dff2f42b774240e8f1ddaee8347baa2ef7d18
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-24 23:07:55 -0500

    feat: get directionStaffTeam Endpoint

---
+++ BaseballStats.WebApi/Endpoints/DirectionStaffTeam/GetDirectionStaffTeamEndpoint.cs
@@ -0,0 +1,21 @@
+using FastEndpoints;
+using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.DirectionStaffTeam.Get;
+
+namespace BaseballStats.WebApi.Endpoints.DirectionStaffTeam;
+
+public class GetDirectionStaffTeamEndpoint : EndpointWithoutRequest<List<DirectionStaffTeamDto>>
+{
+    public override void Configure()
+    {
+        Get("direct");
+        AllowAnonymous();
+        Summary(x => x.Summary = "Get All DirectionStaff-Team");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var response = await new GetDirectionStaffTeamCommand().ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
