ba7056f3ab0368289ceb2fc4eaca87806dcd3361
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-24 23:07:47 -0500

    feat: delete directionStaffTeam Endpoint

---
+++ BaseballStats.WebApi/Endpoints/DirectionStaffTeam/DeleteDirectionStaffTeamEndpoint.cs
@@ -0,0 +1,21 @@
+using FastEndpoints;
+using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.DirectionStaffTeam.Delete;
+
+namespace BaseballStats.WebApi.Endpoints.DirectionStaffTeam;
+
+public class DeleteDirectionStaffTeamEndpoint : Endpoint<DeleteDirectionStaffTeamCommand, DirectionStaffTeamDto>
+{
+    public override void Configure()
+    {
+        Delete("direct/{TeamId}/{DirectionMemberId}");
+        AllowAnonymous();
+        Summary(x => x.Summary = "Delete a DirectionStaff-Team relation");
+    }
+
+    public override async Task HandleAsync(DeleteDirectionStaffTeamCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
