a7e19ed615350105d6b007d299e7f13856a3a49e
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-01-28 15:19:37 -0500

    feat: delete Substitution Endpoint

---
+++ BaseballStats.Application/Features/Game/DeleteSubstitution/DeleteSubstitutionValidator.cs
@@ -0,0 +1,29 @@
+using FastEndpoints;
+using FluentValidation;
+
+namespace BaseballStats.Application.Features.Game.DeleteSubstitution;
+
+public class DeleteSubstitutionValidator : Validator<DeleteSubstitutionCommand>
+{
+    public DeleteSubstitutionValidator()
+    {
+        RuleFor(x => x.GameId)
+            .NotEmpty().WithMessage("GameId is required")
+            .GreaterThan(0).WithMessage("GameId must be greater than 0");
+    
+        RuleFor(x => x.TeamId)
+            .NotEmpty().WithMessage("TeamId is required")
+            .GreaterThan(0).WithMessage("TeamId must be greater than 0");
+    
+        RuleFor(x => x.PlayerInId)
+            .NotEmpty().WithMessage("PlayerInId is required")
+            .GreaterThan(0).WithMessage("PlayerInId must be greater than 0");
+    
+        RuleFor(x => x.PlayerOutId)
+            .NotEmpty().WithMessage("PlayerOutId is required")
+            .GreaterThan(0).WithMessage("PlayerOutId must be greater than 0");
+
+        RuleFor(x => x.Time)
+            .NotEmpty().WithMessage("Time is required");
+    }
+}
