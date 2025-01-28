6300163737d5f7cf7f1cafa3594c3b829167c54e
DHC <dariohcvg@gmail.com>
2025-01-28 16:03:47 -0500

    Fix: Validate Player does not participate in any games (initial alignmnents or substitutions) before player deletion

--- BaseballStats.Application/Features/Player/Delete/DeletePlayerCommandHandler.cs
+++ BaseballStats.Application/Features/Player/Delete/DeletePlayerCommandHandler.cs
@@ -9,6 +9,7 @@ public class DeletePlayerCommandHandler(IUnitOfWork unitOfWork) : CommandHandler
     public override async Task<EmptyResponse> ExecuteAsync(DeletePlayerCommand command, CancellationToken cancellationToken)
     {
         await DatabaseValidations(command);
+        
         var id = command.PlayerId;
 
         var playerRepository = unitOfWork.Repository<Domain.Entities.Player>();
@@ -27,5 +28,25 @@ public class DeletePlayerCommandHandler(IUnitOfWork unitOfWork) : CommandHandler
 
         if (player is null)
             ThrowError("PlayerId not found", StatusCodes.Status404NotFound);
+
+        var alignedPlayerInGame_table = unitOfWork.Repository<Domain.Entities.AlignedPlayerInGame>().DbSet;
+        var gameId = (
+            from apig in alignedPlayerInGame_table
+            where apig.PlayerId == command.PlayerId
+            select apig.GameId
+        ).Cast<long?>().FirstOrDefault();
+
+        if (gameId.HasValue)
+            ThrowError($"Player participates in the initial alignment of at least one game (e.g. game with id {gameId})", StatusCodes.Status400BadRequest);
+        
+        var substitution_table = unitOfWork.Repository<Domain.Entities.Substitution>().DbSet;
+        gameId = (
+            from s in substitution_table
+            where s.PlayerInId == command.PlayerId || s.PlayerOutId == command.PlayerId
+            select s.GameId
+        ).Cast<long?>().FirstOrDefault();
+
+        if (gameId.HasValue)
+            ThrowError($"Player participates in a substitution of at least one game (e.g. game with id {gameId})", StatusCodes.Status400BadRequest);
     }
 }
