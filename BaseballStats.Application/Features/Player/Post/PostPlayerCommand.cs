2364cd549cb040aa60112077432ec1699d798889
DHC <dariohcvg@gmail.com>
2025-01-28 23:09:52 -0500

    Fix: Add Id to player POST

--- BaseballStats.Application/Features/Player/Post/PostPlayerCommand.cs
+++ BaseballStats.Application/Features/Player/Post/PostPlayerCommand.cs
@@ -5,6 +5,7 @@ namespace BaseballStats.Application.Features.Player.Post;
 
 public record PostPlayerCommand : ICommand<RegularPlayerDto>
 {
+    public long Id { get; init; }
     public string Name { get; init; } = null!;
     public int Age { get; init; }
     public int YearsOfExperience { get; init; }
