3036535088e79057b85cdcd527edba8c7e78def1
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-28 21:40:51 -0500

    feat: add put season endpoint

---
+++ BaseballStats.Application/Features/Season/Put/PutSeasonValidator.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace BaseballStats.Application.Features.Season.Put;
+
+public class PutSeasonValidator : Validator<PutSeasonCommand>
+{
+    public PutSeasonValidator()
+    {
+        RuleFor(x => x.SeasonId)
+            .NotEmpty().WithMessage("Season Id is required.")
+            .GreaterThan(0).WithMessage("Season Id must be greater than 0.");
+
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("New Id is required.")
+            .GreaterThan(0).WithMessage("New Id must be greater than 0.");
+    }
+}
---
+++ BaseballStats.WebApi/Endpoints/Season/PutSeasonEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Features.Season.Put;
+using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.Season;
+
+public class PutSeasonEndpoint : Endpoint<PutSeasonCommand, SeasonDto>
+{
+    public override void Configure()
+    {
+        Put("seasons/{SeasonId}");
+        Roles("Admin");
+        Summary(x => x.Summary = "Update a season.");
+    }
+
+    public override async Task HandleAsync(PutSeasonCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
