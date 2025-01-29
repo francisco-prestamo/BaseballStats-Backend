9a9866d69e3319953dd306bc3390b759444805d7
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-01-28 21:40:51 -0500

    feat: add put season endpoint

---
+++ BaseballStats.Application/Features/Season/Put/PutSeasonCommand.cs
@@ -0,0 +1,9 @@
+﻿using BaseballStats.Application.DTOs;
+using FastEndpoints;
+
+namespace BaseballStats.Application.Features.Season.Put;
+
+public record PutSeasonCommand : SeasonDto, ICommand<SeasonDto>
+{
+    public long SeasonId { get; init; }
+};
---
+++ BaseballStats.Application/Features/Season/Put/PutSeasonCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using BaseballStats.Application.DTOs;
+using BaseballStats.Application.Mappers;
+using BaseballStats.Domain.Interfaces.DataAccess;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace BaseballStats.Application.Features.Season.Put;
+
+public class PutSeasonCommandHandler(IUnitOfWork unitOfWork) : CommandHandler<PutSeasonCommand, SeasonDto>
+{
+    public override async Task<SeasonDto> ExecuteAsync(PutSeasonCommand command, CancellationToken ct = new CancellationToken())
+    {
+        await DatabaseValidationsAsync(command);
+
+        await unitOfWork.Repository<Domain.Entities.Season>().DeleteAsync(command.SeasonId);
+        var season = await unitOfWork.Repository<Domain.Entities.Season>().AddAsync(new Domain.Entities.Season() { Id = command.Id });
+
+        await unitOfWork.SaveChangesAsync(ct);
+
+        return season.ToDto();
+    }
+
+    private async Task DatabaseValidationsAsync(PutSeasonCommand command)
+    {
+        var season = await unitOfWork.Repository<Domain.Entities.Season>().GetByIdAsync(command.SeasonId);
+
+        if (season == null)
+            ThrowError("Season not found.", StatusCodes.Status404NotFound);
+
+        season = await unitOfWork.Repository<Domain.Entities.Season>().GetByIdAsync(command.Id);
+
+        if (season is not null)
+            ThrowError($"Season {command.Id} already exists.", StatusCodes.Status400BadRequest);
+    }
+}
