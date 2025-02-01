a8c36e0a1485cb2af388fc1c60241589e2049fee
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-02-01 03:15:18 -0500

    feat: add win teams by series report

---
+++ BaseballStats.Application/Features/Reports/WinTeamsBySeries/SeriesStatsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BaseballStats.Application.Features.Reports.WinTeamsBySeries;
+
+public record SeriesStatsDto
+{
+    public long SerieId { get; init; }
+    public string SerieName { get; init; } = string.Empty;
+    public long TeamId { get; init; }
+    public string TeamWinnerName { get; init; } = string.Empty;
+    public long WinGames { get; init; }
+    public string TechnicalDirector { get; init; } = string.Empty;
+}
---
+++ BaseballStats.Application/Utilities/DocumentTools.cs
@@ -0,0 +1,36 @@
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace BaseballStats.Application.Utilities;
+
+public static class DocumentTools
+{
+    public static Document OpenDocument(string folderName, string fileName, bool landscape = false)
+    {
+        Directory.CreateDirectory(folderName);
+        PdfWriter writer = new($"{folderName}/{fileName}");
+        PdfDocument pdfDocument = new(writer);
+
+        if (landscape)
+        {
+            pdfDocument.SetDefaultPageSize(PageSize.LETTER.Rotate());
+        }
+
+        return new Document(pdfDocument);
+    }
+    
+    public static void AddTitle(this Document document, string title)
+    {
+        document.Add(new Paragraph(title).SetTextAlignment(TextAlignment.CENTER).SetFontSize(20));
+    }
+    
+    public static void AddLine(this Document document, bool isDash = false)
+    {
+        LineSeparator ls = new(isDash ? new DashedLine() : new SolidLine());
+        document.Add(ls);
+    }
+}
