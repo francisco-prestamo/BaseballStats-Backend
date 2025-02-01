093962d14f4d75faebd7df489949dfee86502e01
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2025-02-01 04:21:15 -0500

    feat: add team star players report

---
+++ BaseballStats.WebApi/Endpoints/Reports/TeamStarPlayersEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using BaseballStats.Application.Features.Reports.TeamStarPlayers;
+using FastEndpoints;
+
+namespace BaseballStats.WebApi.Endpoints.Reports;
+
+public class TeamStarPlayersEndpoint : Endpoint<TeamStarPlayersCommand, FileInfo>
+{
+    public override void Configure()
+    {
+        Get("reports/teams/{teamId}/serie/{seasonId}/{seriesId}/star-players");
+        Summary(x => x.Summary = "Genera un reporte de los jugadores estrellas de un equipo que participa en una serie");
+    }
+    
+    public override async Task HandleAsync(TeamStarPlayersCommand command, CancellationToken ct)
+    {
+        var response = await command.ExecuteAsync(ct);
+        await SendFileAsync(response, "application/pdf", cancellation: ct);
+    }
+}
