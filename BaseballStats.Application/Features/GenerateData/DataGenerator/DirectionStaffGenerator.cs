5be09ed8a8671b3ef8f6d8fbb07ca9401ce58927
Eduardo Brito Labrada <eblabrada@gmail.com>
2025-02-03 02:31:10 -0500

    Add DirectionStaffGenerator

---
+++ BaseballStats.Application/Features/GenerateData/DataGenerator/DirectionStaffGenerator.cs
@@ -0,0 +1,21 @@
+namespace BaseballStats.Application.Features.GenerateData.DataGenerator;
+
+public static partial class DataGenerator
+{
+    private static List<Domain.Entities.DirectionStaff> GenerateDirectionStaff(int Amount)
+    {
+        var random = new Random(RandomSeed + 1);
+        var directionStaffs = new List<Domain.Entities.DirectionStaff>();
+        for (int i = 0; i < Amount; i++)
+        {
+            var name = FirstNames[random.Next(0, FirstNames.Count)] + ' ' + LastNames[random.Next(0, LastNames.Count)];
+            var dst = new Domain.Entities.DirectionStaff()
+            {
+                Name = name
+            };
+            directionStaffs.Add(dst);
+        }
+
+        return directionStaffs;
+    }
+}
