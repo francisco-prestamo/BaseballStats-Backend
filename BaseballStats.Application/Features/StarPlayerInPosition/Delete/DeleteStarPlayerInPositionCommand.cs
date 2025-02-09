4c3aba0c7c0cee0ef6dae220309213f22bc225d1
DHC <dariohcvg@gmail.com>
2025-02-09 13:55:55 -0500

    Fix: Change DeleteStarPlayerInPositionComman Ids to long

--- BaseballStats.Application/Features/StarPlayerInPosition/Delete/DeleteStarPlayerInPositionCommand.cs
+++ BaseballStats.Application/Features/StarPlayerInPosition/Delete/DeleteStarPlayerInPositionCommand.cs
@@ -5,8 +5,8 @@ namespace BaseballStats.Application.Features.StarPlayerInPosition.Delete;
 
 public class DeleteStarPlayerInPositionCommand : ICommand<StarPlayerInPositionDto>
 {
-    public int PlayerId { get; set; }
-    public int SeriesId { get; set; }
-    public int SeasonId { get; set; }
+    public long PlayerId { get; set; }
+    public long SeriesId { get; set; }
+    public long SeasonId { get; set; }
     public string Position { get; set; } = null!;
 }
